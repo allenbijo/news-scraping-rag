fbd06f3d2124efbf8d7abdb9c4bc2d8e1fb6c9ce
Allen Bijo <allenbijot@gmail.com>
2024-09-14 00:29:42 +0530

    Vectorstore FAISS

---
+++ vectorsaving.py
@@ -0,0 +1,27 @@
+from langchain_community.vectorstores import FAISS
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_huggingface import HuggingFaceEmbeddings
+import fetch_news
+
+
+# Function to vectorize the news
+def vectorize_papers(news):
+	text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+	splits = text_splitter.split_documents(news)
+	if splits:
+		embeddings = HuggingFaceEmbeddings(model_name="sentence-transformers/all-MiniLM-L6-v2")
+		db = FAISS.from_documents(splits, embeddings)
+
+		# Load existing data
+		try:
+			dbe = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
+			db.merge_from(dbe)
+		except:
+			pass
+		db.save_local("faiss_index")
+
+
+if __name__ == '__main__':
+	links = fetch_news.get_news_links()
+	news = fetch_news.load_news(links)
+	vectorize_papers(news)
